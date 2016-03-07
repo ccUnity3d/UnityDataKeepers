4755f80e0a92538cc8a11cbd7e03292fb3620f7a
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-03-07 19:02:16 +0200

    SQLite and data base creating

---
+++ UnityDataKeepersProj/Assets/Plugins/DataKeepers/Scripts/DataKeepersDB.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+using System.IO;
+using Mono.Data.Sqlite;
+
+// ReSharper disable once InconsistentNaming
+namespace DataKeepers
+{
+    public class DataKeepersDB
+    {
+        private string _constr = "";
+        private IDbConnection _dbc;
+        private IDbCommand _dbcm;
+
+        private static DataKeepersDB _instance;
+        public static DataKeepersDB Instance { get { return _instance ?? (_instance = new DataKeepersDB()); } }
+
+        public IDbConnection Connect()
+        {
+            if (!DataKeepersPaths.Exists)
+            {
+                CreateEmpty();
+            }
+            _constr = string.Format("URI=file:{0}", DataKeepersPaths.DataBasePath);
+            _dbc = new SqliteConnection(_constr);
+            _dbc.Open();
+            _dbcm = _dbc.CreateCommand();
+            return _dbc;
+        }
+
+        public IDbCommand GetCommand()
+        {
+            return _dbcm;
+        }
+
+        private void CreateEmpty()
+        {
+            var dir = Path.GetDirectoryName(DataKeepersPaths.DataBasePath);
+            if (dir != null && !Directory.Exists(dir))
+            {
+                Directory.CreateDirectory(dir);
+            }
+            SqliteConnection.CreateFile(DataKeepersPaths.DataBasePath);
+        }
+    }
+}
---
+++ UnityDataKeepersProj/Assets/Scripts/Tests/InitTest.cs
@@ -0,0 +1,16 @@
+﻿using DataKeepers;
+using UnityEngine;
+
+public class InitTest : MonoBehaviour
+{
+    [ContextMenu("Connection test")]
+    public void Test()
+    {
+        Debug.Log(string.Format("Path of db: {0}",DataKeepersPaths.DataBasePath));
+        Debug.Log(string.Format("Is db exists? {0}", DataKeepersPaths.Exists));
+
+        Debug.Log("Connection...");
+        var conn = DataKeepersDB.Instance.Connect();
+        Debug.Log("Connected? "+(conn!=null));
+    }
+}
