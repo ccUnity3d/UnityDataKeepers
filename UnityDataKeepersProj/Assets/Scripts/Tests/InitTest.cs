b9979c07d1f8463e8bb90196d23f8304972cf16f
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-03-10 19:54:08 +0200

    update test example

--- UnityDataKeepersProj/Assets/Scripts/Tests/InitTest.cs
+++ UnityDataKeepersProj/Assets/Scripts/Tests/InitTest.cs
@@ -1,13 +1,27 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class InitTest : MonoBehaviour
 {
+    public Text TestLog;
+
     void Awake()
     {
         var id = "text1";
         var loc = LocalizationKeeper.Instance.GetById(id);
-        Debug.LogFormat("localization for id {0}: {1}", id, loc == null ? "null" : loc.Justify());
-        Debug.LogFormat("resource for id {0}: {1}", "wood", ResourcesKeeper.Instance.GetById("wood"));
-        Debug.LogFormat("resource for id {0}: {1}", "wood2", ResourcesKeeper.Instance.GetById("wood2"));
+        TestLog.text+=string.Format("localization for id {0}: {1}", id, loc == null ? "null" : loc.Justify());
+        TestLog.text += string.Format("resource for id {0}: {1}", "wood", ResourcesKeeper.Instance.GetById("wood"));
+        TestLog.text += string.Format("resource for id {0}: {1}", "wood2", ResourcesKeeper.Instance.GetById("wood2"));
+
+        var res = ResourcesKeeper.Instance.GetById("wood");
+        res.DebugDescription += ((int) (Random.value*10)).ToString();
+        ResourcesKeeper.Instance.Update(res);
+        TestLog.text += string.Format("resource for id {0}: {1}", "wood", ResourcesKeeper.Instance.GetById("wood"));
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
