5a15dbf5b67db8d332023c1b6999170815f19b1c
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-03-17 19:51:16 +0200

    updating project

--- UnityDataKeepersProj/Assets/Scripts/Tests/InitTest.cs
+++ UnityDataKeepersProj/Assets/Scripts/Tests/InitTest.cs
@@ -7,42 +7,42 @@ using Random = UnityEngine.Random;
 
 public class InitTest : MonoBehaviour
 {
-    public Text TestLog;
-
-    private int _t = 0;
-    private int _num = 0;
-
-    void Awake()
-    {
-        CustomKeeper.Instance.GetById("test1");
-        CustomKeeper.Instance.Add(new CustomKeeperItem() {Id = Random.value.ToString(), SomeData = "data"});
-        Debug.Log(CustomKeeper.Instance.Count());
-
-        StartCoroutine(TestProc());
-    }
-
-    IEnumerator TestProc()
-    {
-        while (true)
-        {
-            EffectsKeeper.Instance.FindAll(e => e.Id == "depot_get" && e.ResultType == "Add");
-            _num++;
-            yield return null;
-        }
-    }
-
-    void Update()
-    {
-        if ((int) (Time.time) > _t)
-        {
-            _t = (int) (Time.time);
-            Debug.Log(_num);
-            _num = 0;
-        }
-    }
-
-    public void Restart()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+//    public Text TestLog;
+//
+//    private int _t = 0;
+//    private int _num = 0;
+//
+//    void Awake()
+//    {
+//        CustomKeeper.Instance.GetById("test1");
+//        CustomKeeper.Instance.Add(new CustomKeeperItem() {Id = Random.value.ToString(), SomeData = "data"});
+//        Debug.Log(CustomKeeper.Instance.Count());
+//
+//        StartCoroutine(TestProc());
+//    }
+//
+//    IEnumerator TestProc()
+//    {
+//        while (true)
+//        {
+//            EffectsKeeper.Instance.FindAll(e => e.Id == "depot_get" && e.ResultType == "Add");
+//            _num++;
+//            yield return null;
+//        }
+//    }
+//
+//    void Update()
+//    {
+//        if ((int) (Time.time) > _t)
+//        {
+//            _t = (int) (Time.time);
+//            Debug.Log(_num);
+//            _num = 0;
+//        }
+//    }
+//
+//    public void Restart()
+//    {
+//        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+//    }
 }
