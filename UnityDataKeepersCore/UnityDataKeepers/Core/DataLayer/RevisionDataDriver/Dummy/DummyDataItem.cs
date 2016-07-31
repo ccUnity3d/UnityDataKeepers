a3b4cb4202da1c90958b71707a1b9390916ded39
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-07-31 19:18:14 +0300

    dummy item improvement

--- UnityDataKeepersCore/UnityDataKeepers/Core/DataLayer/RevisionDataDriver/Dummy/DummyDataItem.cs
+++ UnityDataKeepersCore/UnityDataKeepers/Core/DataLayer/RevisionDataDriver/Dummy/DummyDataItem.cs
@@ -1,9 +1,19 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace UnityDataKeepersCore.Core.DataLayer.RevisionDataDriver
 {
-    public class DummyDataItem : IDataItem
+    public class DummyDataItem : IDataItem,
+        IComparer<DummyDataItem>
     {
         public Hash128 Hash { get; private set; }
+        public string DummyProperty;
+        public int Compare(DummyDataItem x, DummyDataItem y)
+        {
+            return string.Compare(x.Hash.ToString(),
+                y.Hash.ToString(),
+                StringComparison.InvariantCulture);
+        }
     }
 }
