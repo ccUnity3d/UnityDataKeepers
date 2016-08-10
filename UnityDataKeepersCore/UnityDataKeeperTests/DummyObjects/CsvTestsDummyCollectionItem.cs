23b71be26977f0d0ea3109c4219cbe050fe8b5b2
Radomyr <radomyrslaboshpitskyi@gmail.com>
2016-08-11 00:28:08 +0300

    dummy item for csv tests

--- UnityDataKeepersCore/UnityDataKeeperTests/DummyObjects/CsvTestsDummyCollectionItem.cs
+++ UnityDataKeepersCore/UnityDataKeeperTests/DummyObjects/CsvTestsDummyCollectionItem.cs
@@ -1,23 +1,46 @@
 ﻿using System;
-using UnityDataKeepersCore.Core.DataLayer.DataCollectionDrivers;
 using UnityDataKeepersCore.Core.DataLayer.Model;
 using UnityEngine;
+using Random = System.Random;
 
 namespace UnityDataKeeperTests.DummyObjects
 {
-    class CsvTestsDummyCollectionItem
+    class CsvTestsDummyCollectionItem : IDataItem
     {
-        public class DummyDataItem : IDataItem
-        {
-            public Hash128 Hash { get; private set; }
-            public string DummyProperty;
+        private static readonly Random Random = new Random();
 
-            public int CompareTo(object obj)
-            {
-                return string.Compare(Hash.ToString(),
-                    ((UnityDataKeepersCore.Core.DataLayer.DataCollectionDrivers.DummyDataItem)obj).Hash.ToString(),
-                    StringComparison.InvariantCulture);
-            }
+        public enum CsvTestEnum
+        {
+            Field1,
+            Field2,
+            OtherField
+        }
+
+        public Hash128 Hash { get; private set; }
+        public string StringProperty;
+        public int IntProperty;
+        public float FloatProperty;
+        public CsvTestEnum EnumField;
+        public DateTime DateTimeField;
+        public TimeSpan TimeSpanField;
+
+        public CsvTestsDummyCollectionItem()
+        {
+            Hash = new Hash128(RndUint(), RndUint(), RndUint(), RndUint());
+        }
+
+        private static uint RndUint()
+        {
+            var thirtyBits = (uint) Random.Next(1 << 30);
+            var twoBits = (uint) Random.Next(1 << 2);
+            return (thirtyBits << 2) | twoBits;
+        }
+
+        public int CompareTo(object obj)
+        {
+            return string.Compare(Hash.ToString(),
+                ((CsvTestsDummyCollectionItem)obj).Hash.ToString(),
+                StringComparison.InvariantCulture);
         }
     }
 }
